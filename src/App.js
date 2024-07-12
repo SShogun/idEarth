99939a56478057e71290bfe4881b7a67415eb733
SShogun <sohamshirke.215099@gmail.com>
2024-07-12 21:43:43 +0530

    Made logo in Figma

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
-
+import Research from "./pages/Research";
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/" element={<Research />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import "../App.css";
-
 import PrimaryButton from "./PrimaryButton";
+import { NavLink } from "react-router-dom";
+import logo from "../images/logo2_bg.png";
 export default function Navbar() {
   return (
     <nav>
-      <div className="navText">
-        <p className="navTitle dm-sans-500">idEarth</p>
+      <div className="navLogo">
+        <img src={logo} height={120} />
       </div>
       <div className="navLinks">
         <ul>
           <li>
-            <a>Home</a>
+            <NavLink exact to="/" className="link">
+              Home
+            </NavLink>
           </li>
           <li>
-            <a>Research</a>
+            <NavLink to="/research" className="link">
+              Research
+            </NavLink>
           </li>
           <li>
-            <a>About Us</a>
+            <NavLink className="link" to="/about">
+              About
+            </NavLink>
           </li>
           <li>
             <PrimaryButton>More</PrimaryButton>
